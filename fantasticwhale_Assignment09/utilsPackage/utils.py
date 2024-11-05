52dfdc355dae916fca77bf3c61cb17985da2b5ae
kiraimae <tipkemam@LCB-06>
2024-11-05 13:38:11 -0500

    utils Package - Connection to sql server

--- fantasticwhale_Assignment09/utilsPackage/utils.py
+++ fantasticwhale_Assignment09/utilsPackage/utils.py
@@ -1,2 +1,16 @@
 #utils
 
+import pyodbc
+
+def get_db_connection():
+    try:
+        conn = pyodbc.connect(
+            'driver={SQL Server};'
+            'server=lcb-sql.uccob.uc.edu\\nicholdw;'
+            'database=grocerystoresimulator;'
+            'uid=IS4010Login;'
+            'pwd=P@ssword2;')
+    except:
+        conn = None
+
+    return conn
