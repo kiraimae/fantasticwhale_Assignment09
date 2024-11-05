cf685b7393e61298544f48decc873676e1b18f71
Jared Rababy <jaredrababy@gmail.com>
2024-11-05 13:49:34 -0500

    Main package code

--- fantasticwhale_Assignment09/mainPackage/main.py
+++ fantasticwhale_Assignment09/mainPackage/main.py
@@ -1,2 +1,17 @@
 
 #main.py
+
+from utilsPackage.utils import get_db_connection  # Importing the connection function from utils
+import pyodbc #For Question 2
+import random
+
+
+try:
+    conn= get_db_connection()
+#Submits sql to database and stores in cursor
+    cursor = conn.cursor() #Temporary table
+    cursor.execute('SELECT * FROM tProduct')
+except Exception as e:
+    print("Error accessing database")
+    print(e)
+    exit() #Give up
